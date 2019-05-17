b78f51b68959dd10effc3eca4fd3e1c94e650d32
Johny.li <johny.li@selmit.com>
2019-05-17 18:38:02 +0800

    update

--- ch16-traits/src/main.rs
+++ ch16-traits/src/main.rs
@@ -30,6 +30,26 @@ impl Drop for Seconds {
     }
 }
 
+struct Fibonacci {
+    curr: u32,
+    next: u32,
+}
+
+impl Iterator for Fibonacci {
+    type Item = u32;
+    fn next(&mut self) ->Option<u32> {
+        let new_next = self.curr + self.next;
+
+        self.curr = self.next;
+        self.next = new_next;
+        Some(self.curr)
+    }
+}
+
+fn fibonacci() -> Fibonacci {
+    Fibonacci { curr: 1, next: 1}
+}
+
 fn main() {
     let _one_second = Seconds(1);
     let two_seconds = Seconds(2);
@@ -49,4 +69,24 @@ fn main() {
         "bigger"
     };
     println!("One foot is {} than one meter", cmp);
+
+    println!("======================================");
+
+    let mut sequence = 0..3;
+
+    println!("Four consecutive `next` calls on 0..3");
+    println!("> {:?}", sequence.next());
+    println!("> {:?}", sequence.next());
+    println!("> {:?}", sequence.next());
+    println!("> {:?}", sequence.next());
+    println!("> {:?}", sequence.next());
+
+    println!("Iterate through 0..3 using `for`");
+    for i in 0..3 {
+        println!("> {}", i);
+    }
+    println!("The next four terms of the Fibonacci sequence are: ");
+    for i in fibonacci().skip(4).take(4) {
+        println!("> {}", i);
+    }
 }
