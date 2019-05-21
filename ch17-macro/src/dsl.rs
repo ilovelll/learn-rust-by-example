97b5a9b33a18e292748cbbb3dd02c96513dd1e15
Johny.li <johny.li@selmit.com>
2019-05-21 18:25:08 +0800

    update

---
+++ ch17-macro/src/dsl.rs
@@ -0,0 +1,28 @@
+macro_rules! calculate {
+  (eval $e:expr) => {{
+    {
+      let val: usize = $e;
+      println!("{} = {}", stringify!{$e}, val);
+    }
+  }};
+
+  (eval $e: expr, $(eval $es:expr),+) => {{
+    calculate! { eval $e }
+    calculate! { $(eval $es),+ }
+  }};
+}
+
+fn main() {
+  calculate! {
+    eval 1 + 2
+  }
+
+  calculate! {
+    eval (1 + 2) * (3 / 4)
+  }
+
+  calculate! [
+    eval 1 + 4,
+    eval 2 + 4
+  ]
+}
