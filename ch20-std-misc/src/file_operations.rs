84ef0ad23a5b51610e1836dfce206bfe30ba39ee
Johny.li <johny.li@selmit.com>
2019-05-30 18:58:06 +0800

    update

--- ch20-std-misc/src/file_operations.rs
+++ ch20-std-misc/src/file_operations.rs
@@ -7,81 +7,79 @@ use std::os::windows;
 use std::path::Path;
 
 fn cat(path: &Path) -> io::Result<String> {
-  let mut f = File::open(path)?;
-  let mut s = String::new();
-  match f.read_to_string(&mut s) {
-      Ok(_) => Ok(s),
-      Err(e) => Err(e),
-  }
+    let mut f = File::open(path)?;
+    let mut s = String::new();
+    f.read_to_string(&mut s)?;
+    Ok(s)
 }
 
 fn echo(s: &str, path: &Path) -> io::Result<()> {
-  let mut f = File::create(path)?;
-  f.write_all(s.as_bytes())
+    let mut f = File::create(path)?;
+    f.write_all(s.as_bytes())
 }
 
 fn touch(path: &Path) -> io::Result<()> {
-  match OpenOptions::new().create(true).write(true).open(path) {
-    Ok(_) => Ok(()),
-    Err(e) => Err(e),
-  }
+    OpenOptions::new().create(true).write(true).open(path)?;
+    Ok(())
 }
 
-fn main () {
-  println!("`mkdir` a`");
-  match fs::create_dir("a") {
-    Err(why) => println!("! {:?}", why.kind()),
-    Ok(_) => {},
-  }
+fn main() {
+    println!("`mkdir` a`");
+    match fs::create_dir("a") {
+        Err(why) => println!("! {:?}", why.kind()),
+        Ok(_) => {}
+    }
 
-  println!("`echo hello > a/b.txt`");
-  echo("hello", &Path::new("a/b.txt")).unwrap_or_else(|why| {
-    println!("! {:?}", why.kind());
-  });
-
-  println!("`mkdir -p a/c/d`");
-  fs::create_dir_all("a/c/d").unwrap_or_else(|why| {
-    println!("! {:?}", why.kind());
-  });
-
-  println!("`touch a/c/e.txt`");
-  touch(&Path::new("a/c/e.txt")).unwrap_or_else(|why| {
-    println!("! {:?}", why.kind());
-  });
-
-  println!("`ln -s a/b.txt a/c/b.txt`");
-  // if cfg!(target_family = "unix") {
-  //   unix::fs::symlink("a/b.txt", "a/c/b.txt").unwrap_or_else(|why| {
-  //     println!("! {:?}", why.kind());
-  //   });
-  // } else 
-  if cfg!(target_family = "windows") {
-    windows::fs::symlink_file("a/b.txt", "a/c/b.txt").unwrap_or_else(|why| {
-      println!("! {:?}", why);
+    println!("`echo hello > a/b.txt`");
+    echo("hello", &Path::new("a/b.txt")).unwrap_or_else(|why| {
+        println!("! {:?}", why.kind());
     });
-  }
 
-  println!("`cat a/c/b.txt`");
-  match cat(&Path::new("a/c/b.txt")) {
-    Err(why) => println!("! {:?}", why.kind()),
-    Ok(s) => println!("> {}", s),
-  }
+    println!("`mkdir -p a/c/d`");
+    fs::create_dir_all("a/c/d").unwrap_or_else(|why| {
+        println!("! {:?}", why.kind());
+    });
 
-  println!("`ls a`");
-  match fs::read_dir("a") {
-    Err(why) => println!("! {:?}", why.kind()),
-    Ok(paths) => for path in paths {
-      println!("> {:?}", path.unwrap().path());
-    },
-  }
+    println!("`touch a/c/e.txt`");
+    touch(&Path::new("a/c/e.txt")).unwrap_or_else(|why| {
+        println!("! {:?}", why.kind());
+    });
 
-  println!("`rm a/c/e.txt");
-  fs::remove_file("a/c/e.txt").unwrap_or_else(|why| {
-    println!("! {:?}", why.kind());
-  });
+    println!("`ln -s a/b.txt a/c/b.txt`");
+    // if cfg!(target_family = "unix") {
+    //   unix::fs::symlink("a/b.txt", "a/c/b.txt").unwrap_or_else(|why| {
+    //     println!("! {:?}", why.kind());
+    //   });
+    // } else
+    if cfg!(target_family = "windows") {
+        windows::fs::symlink_file("a/b.txt", "a/c/b.txt").unwrap_or_else(|why| {
+            println!("! {:?}", why);
+        });
+    }
 
-  println!("`rmdir a/c/d`");
-  fs::remove_dir("a/c/d").unwrap_or_else(|why| {
-    println!("! {:?}", why.kind());
-  });
+    println!("`cat a/c/b.txt`");
+    match cat(&Path::new("a/c/b.txt")) {
+        Err(why) => println!("! {:?}", why.kind()),
+        Ok(s) => println!("> {}", s),
+    }
+
+    println!("`ls a`");
+    match fs::read_dir("a") {
+        Err(why) => println!("! {:?}", why.kind()),
+        Ok(paths) => {
+            for path in paths {
+                println!("> {:?}", path.unwrap().path());
+            }
+        }
+    }
+
+    println!("`rm a/c/e.txt");
+    fs::remove_file("a/c/e.txt").unwrap_or_else(|why| {
+        println!("! {:?}", why.kind());
+    });
+
+    println!("`rmdir a/c/d`");
+    fs::remove_dir("a/c/d").unwrap_or_else(|why| {
+        println!("! {:?}", why.kind());
+    });
 }
